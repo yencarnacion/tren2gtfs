4131442a5d99844d4671960d4147a3a44673f780
Yamir Encarnacion <yencarnacion@przone.org>
2013-06-16 19:10:37 -0400

    -Refactored

---
+++ src/tren2gtfs/tred2gtfslib.groovy
@@ -0,0 +1,325 @@
+package tren2gtfs
+@Grab(
+        group='joda-time',
+        module='joda-time',
+        version='2.2'
+)
+@GrabExclude('xml-apis:xml-apis')
+import org.joda.time.DateTimeZone
+import org.joda.time.DateTime
+
+class Agency {
+    String agencyId
+    String agencyName
+    String agencyUrl
+    String agencyTimezone
+    String agencyLang
+    String agencyPhone
+    String agencyFareUrl
+}
+
+class Route {
+    String routeId
+    String agencyId
+    String routeShortName
+    String routeLongName
+    String routeDesc
+    String routeType
+    String routeUrl
+    String routecolor
+    String routeTextColor
+}
+
+
+class Stop {
+    String codeName
+    String stopId
+    String stopCode
+    String stopName
+    String stopDesc
+    String stopLat
+    String stopLon
+    String zoneId
+    String stopUrl
+    String locationType
+    String parentStation
+    String stopTimezone
+    String wheelchairBoarding
+}
+
+class Trip {
+    Stop startStop
+    Stop endStop
+    String routeId
+    String serviceId
+    String tripId
+    String tripHeadsign
+    String tripShortName
+    String directionId
+    String blockId
+    String shapeId
+    String wheelchairAccessible
+}
+
+class TrainArrival {
+    String arrivesTo
+    String direction
+    String daytype
+    String arrivalTime
+}
+
+class StopTime {
+    String tripId
+    String arrivalTime
+    String departureTime
+    String stopId
+    String stopSequence
+}
+
+class TrainLocalTime implements Comparable<TrainLocalTime> {
+    int trainLocalHourI
+    int trainLocalMinuteI
+    int trainLocalSecondI
+    boolean nextDay = false
+    String trainLocalTimeZone = "America/Puerto_Rico"
+    TrainLocalTime(String trainLocalTimeS){
+        def (hour, minute, second) = trainLocalTimeS.split(":")
+
+        int hourInt = ((String) hour).toInteger()
+        int minuteInt = ((String) minute).toInteger()
+        int secondInt = ((String) second).toInteger()
+        nextDay = false
+
+        if(hourInt>=21 && hourInt <24){
+            trainLocalHourI = 8-(24-hourInt)
+            trainLocalMinuteI = minuteInt
+            trainLocalSecondI = secondInt
+            nextDay = false
+        } else if (hourInt>=0 && hourInt < 16){
+            trainLocalHourI = 8 + hourInt
+            trainLocalMinuteI = minuteInt
+            trainLocalSecondI = secondInt
+            nextDay = false
+        } else if (hourInt>=16 && hourInt < 21){
+            trainLocalHourI = hourInt - 16
+            trainLocalMinuteI = minuteInt
+            trainLocalSecondI = secondInt
+            nextDay = true
+        } else {
+            throw new Exception ("Unknown hour: "+trainLocalTimeS)
+        }
+    }
+
+    // HH:mm:ss
+    String toString(String pattern="HH:mm:ss"){
+
+        DateTime jdt = new DateTime(DateTimeZone.forID(trainLocalTimeZone));
+        DateTime ljdt = new DateTime(
+                jdt.getYear(),
+                jdt.getMonthOfYear(),
+                jdt.getDayOfMonth(),
+                trainLocalHourI,
+                trainLocalMinuteI,
+                trainLocalSecondI,
+                DateTimeZone.forID(trainLocalTimeZone)
+        )
+        def retString
+        if(nextDay){
+            def timePattern = ljdt.toString(pattern)
+            def (h, m, s) = timePattern.split(":")
+            def hi = ((String) h).toInteger()+24
+            retString = "${hi}:${m}:${s}"
+        } else {
+            retString = ljdt.toString(pattern)
+        }
+        return retString
+    }
+
+    int compareTo(TrainLocalTime o){
+
+        DateTime jdt = new DateTime(DateTimeZone.forID(trainLocalTimeZone));
+        DateTime ljdt = new DateTime(
+                jdt.getYear(),
+                jdt.getMonthOfYear(),
+                jdt.getDayOfMonth(),
+                trainLocalHourI,
+                trainLocalMinuteI,
+                trainLocalSecondI,
+                DateTimeZone.forID(trainLocalTimeZone)
+        )
+        if(nextDay){
+            ljdt = ljdt.plusDays(1)
+        }
+
+        DateTime ojdt = new DateTime(
+                jdt.getYear(),
+                jdt.getMonthOfYear(),
+                jdt.getDayOfMonth(),
+                o.trainLocalHourI,
+                o.trainLocalMinuteI,
+                o.trainLocalSecondI,
+                DateTimeZone.forID(trainLocalTimeZone)
+        )
+        if(o.nextDay){
+            ojdt = ojdt.plusDays(1)
+        }
+
+        int comparison = ljdt.compareTo(ojdt)
+        return comparison
+    }
+
+    boolean isAfter(TrainLocalTime o){
+        DateTime jdt = new DateTime(DateTimeZone.forID(trainLocalTimeZone));
+        DateTime ljdt = new DateTime(
+                jdt.getYear(),
+                jdt.getMonthOfYear(),
+                jdt.getDayOfMonth(),
+                trainLocalHourI,
+                trainLocalMinuteI,
+                trainLocalSecondI,
+                DateTimeZone.forID(trainLocalTimeZone)
+        )
+        if(nextDay){
+            ljdt = ljdt.plusDays(1)
+        }
+
+        DateTime ojdt = new DateTime(
+                jdt.getYear(),
+                jdt.getMonthOfYear(),
+                jdt.getDayOfMonth(),
+                o.trainLocalHourI,
+                o.trainLocalMinuteI,
+                o.trainLocalSecondI,
+                DateTimeZone.forID(trainLocalTimeZone)
+        )
+        if(o.nextDay){
+            ojdt = ojdt.plusDays(1)
+        }
+
+        int comparison = ljdt.compareTo(ojdt)
+        return (comparison > 0)
+    }
+}
+
+class TrainSchedule {
+    private def trainScheduleFileName
+    private def trainArrivals = []
+
+    TrainSchedule(file){
+        trainScheduleFileName = "${file}"
+        readTrainScheduleFile()
+        this
+    }
+
+    private def readTrainScheduleFile() {
+        def trainScheduleFile = new File (trainScheduleFileName)
+
+        trainScheduleFile.eachLine() { line, lineNo ->
+            if (lineNo>1) {
+                line = line.replace("\"","")
+                def( arrivesTo, direction, daytipe, arrivalTime) = line.split(",")
+                TrainArrival trainArrival = new TrainArrival(
+                        arrivesTo: ((String) arrivesTo).toLowerCase(),
+                        direction: ((String) direction).toLowerCase(),
+                        daytype: ((String) daytipe).toLowerCase(),
+                        arrivalTime: ((String) arrivalTime).toLowerCase()
+                )
+                trainArrivals << trainArrival
+            }
+        }
+
+        println "Read ${trainArrivals.size()} schedules"
+        /*for (TrainArrival t: trainArrivals){
+            println t.dump();
+        }*/
+        return trainArrivals
+    }
+
+    /**
+     * Reads stop times from http://www.dtop.gov.pr/itinerario.asp
+     */
+    def readScheduleFromTrainArrivals(){
+
+        def stops = StopCollection.getTheStops()
+
+        def directions = ['to_bayamon', 'to_sagrado']
+
+        int i=1;
+        for (String direction: directions) {
+            for(int startStationId = 1; startStationId <= stops.size(); startStationId++){
+                def startStopStation = StopCollection.getStopFromId(startStationId)
+                for(int stopStationId = 1; stopStationId <= stops.size(); stopStationId++){
+
+                    //For Testing
+                    if(!(((direction == 'to_sagrado')&& (startStationId==1) && (stopStationId==16)) ||
+                            ((direction == "to_bayamon") && (startStationId==16) && (stopStationId==1)))
+                    )
+                        continue;
+
+
+                    if((direction == 'to_sagrado' && (startStationId < stopStationId) ) ||
+                            (direction == 'to_bayamon' && (startStationId > stopStationId))
+                    ){
+                        def stopStopStation = StopCollection.getStopFromId(stopStationId)
+                        def dayTypes = ["LOW_SEASON_WORKDAY","RESTDAY","WORKDAY"]
+                        dayTypes.each({daytype ->
+                            def leavingTimes = getStopTrainScheduleTimes(startStopStation, direction, daytype)
+                            leavingTimes.each({ leavingTime ->
+                                def arrivalTime = getStopTrainArrivalTime(startStationId, direction, leavingTime, stopStationId, daytype)
+                                println ">>>>>>>"
+                                println "[${i++}]-${daytype}-(${direction}) From ${startStopStation.stopName} To ${stopStopStation.stopName}"
+                                println "Leaves: ${leavingTime} and Arrives: ${arrivalTime}"
+                                println "<<<<<<<"
+                            })
+                        })
+                    }
+                }
+            }
+        }
+    }
+
+    def getStopTrainArrivalTime(int fromStationId, direction, fromTrainTime, int toStationId, daytype){
+        def stops = StopCollection.getTheStops()
+
+        if(direction=="to_bayamon"){
+            assert(fromStationId>toStationId)
+            int nextStation = fromStationId-1
+            def nextStationSchedule = getStopTrainScheduleTimes(StopCollection.getStopFromId(nextStation), direction, daytype)
+            def nextStationArrival = nextStationSchedule.find({if(it.isAfter(fromTrainTime)) it})
+            while(nextStation>toStationId){
+                nextStation = nextStation - 1
+                nextStationSchedule = getStopTrainScheduleTimes(StopCollection.getStopFromId(nextStation), direction, daytype)
+                nextStationArrival = nextStationSchedule.find({if(it.isAfter(nextStationArrival)) it})
+            }
+            return nextStationArrival
+        }
+
+        if(direction=="to_sagrado"){
+            assert (toStationId>fromStationId)
+            int nextStation = fromStationId+1
+            def nextStationSchedule = getStopTrainScheduleTimes(StopCollection.getStopFromId(nextStation), direction, daytype)
+            def nextStationArrival = nextStationSchedule.find({if(it.isAfter(fromTrainTime)) it})
+            while(nextStation<toStationId){
+                nextStation = nextStation + 1
+                nextStationSchedule = getStopTrainScheduleTimes(StopCollection.getStopFromId(nextStation), direction, daytype)
+                nextStationArrival = nextStationSchedule.find({if(it.isAfter(nextStationArrival)) it})
+            }
+            return nextStationArrival
+        }
+
+        assert false
+
+    }
+
+    def getStopTrainScheduleTimes(station, direction, daytype){
+        def stationCodeName = station.codeName
+        assert(stationCodeName!=null)
+        trainArrivals.findAll({trainArrival ->
+            if(((String) trainArrival.arrivesTo).toUpperCase() == stationCodeName.toUpperCase() &&
+                    trainArrival.direction.toUpperCase() == direction.toUpperCase() &&
+                    trainArrival.daytype.toUpperCase() == daytype.toUpperCase() )
+                trainArrival
+        }).collect({new TrainLocalTime(it.arrivalTime)}).sort()
+    }
+}
+
--- src/tren2gtfs/tren2gtfs.groovy
+++ src/tren2gtfs/tren2gtfs.groovy
@@ -2,8 +2,7 @@ package tren2gtfs
 /**
  * Created by yamir on 6/6/13.
  */
-@Grab( 'net.sourceforge.nekohtml:nekohtml:1.9.18' )
-@GrabExclude('xml-apis:xml-apis')
+
 import java.util.zip.ZipOutputStream
 import java.util.zip.ZipEntry
 
@@ -16,6 +15,301 @@ import java.util.zip.ZipEntry
  agency_phone = null
  agency_fare_url = null
  trainScheduleFileName =  "trainschedule.csv"
+
+
+class StopCollection {
+    private static def stops = []
+
+    static def getTheStops(){
+        if(stops.size()==0){
+            populateStops()
+        }
+        return stops
+    }
+
+    static def getStopFromId(stationId) {
+        stops.find({stop -> if(stop.stopId == "${stationId}")  stop})
+    }
+
+    private static def populateStops(){
+        def sagrado_corazon = new Stop(
+                codeName: "sagrado_corazon",
+                stopId: "16",
+                stopCode: "",
+                stopName: "Sagrado Coraz\u00F3n",
+                stopDesc: "",
+                stopLat:"-66.05968D",
+                stopLon: "18.437642D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def hato_rey = new Stop(
+                codeName: "hato_rey",
+                stopId: "15",
+                stopCode: "",
+                stopName: "Hato Rey",
+                stopDesc: "",
+                stopLat:"-66.059985D",
+                stopLon: "18.430277D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def roosevelt = new Stop(
+                codeName: "roosevelt",
+                stopId: "14",
+                stopCode: "",
+                stopName: "Roosevelt",
+                stopDesc: "",
+                stopLat:"-66.058779D",
+                stopLon: "18.424528D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def domenech = new Stop(
+                codeName: "domenech",
+                stopId: "13",
+                stopCode: "",
+                stopName: "D\u00F3menech",
+                stopDesc: "",
+                stopLat:"-66.056338D",
+                stopLon: "18.416346D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def pinero = new Stop(
+                codeName: "pinero",
+                stopId: "12",
+                stopCode: "",
+                stopName: "Pi\u00F1ero",
+                stopDesc: "",
+                stopLat:"-66.055225D",
+                stopLon: "18.411693D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def universidad = new Stop(
+                codeName: "universidad",
+                stopId: "11",
+                stopCode: "",
+                stopName: "Universidad",
+                stopDesc: "",
+                stopLat:"-66.05193D",
+                stopLon: "18.406024D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def rio_piedras = new Stop (
+                codeName: "rio_piedras",
+                stopId: "10",
+                stopCode: "",
+                stopName: "Cupey",
+                stopDesc: "",
+                stopLat:"-66.063209D",
+                stopLon: "18.392124D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+        def cupey = new Stop (
+                codeName: "cupey",
+                stopId: "9",
+                stopCode: "",
+                stopName: "R\u00EDo Piedras",
+                stopDesc: "",
+                stopLat:"-66.052413D",
+                stopLon: "18.400251D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def centro_medico = new Stop (
+                codeName: "centro_medico",
+                stopId: "8",
+                stopCode: "",
+                stopName: "Centro M\u00E9dico",
+                stopDesc: "",
+                stopLat:"-66.075734D",
+                stopLon: "18.393167D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def san_francisco = new Stop (
+                codeName: "san_francisco",
+                stopId: "7",
+                stopCode: "",
+                stopName: "San Francisco",
+                stopDesc: "",
+                stopLat:"-66.084036D",
+                stopLon: "18.392307D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def las_lomas = new Stop (
+                codeName: "las_lomas",
+                stopId: "6",
+                stopCode: "",
+                stopName: "Las Lomas",
+                stopDesc: "",
+                stopLat:"-66.095176D",
+                stopLon: "18.392398D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def martinez_nadal = new Stop (
+                codeName: "martinez_nadal",
+                stopId: "5",
+                stopCode: "",
+                stopName: "Mart\u00EDnez Nadal",
+                stopDesc: "",
+                stopLat:"-66.102911D",
+                stopLon: "18.392811D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def torrimar = new Stop (
+                codeName: "torrimar",
+                stopId: "4",
+                stopCode: "",
+                stopName: "Torrimar",
+                stopDesc: "",
+                stopLat:"-66.120175D",
+                stopLon: "18.393597D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+
+        def jardines = new Stop (
+                codeName: "jardines",
+                stopId: "3",
+                stopCode: "",
+                stopName: "Jardines",
+                stopDesc: "",
+                stopLat:"-66.128919D",
+                stopLon: "18.395734D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def deportivo = new Stop (
+                codeName: "deportivo",
+                stopId: "2",
+                stopCode: "",
+                stopName: "Deportivo",
+                stopDesc: "",
+                stopLat:"-66.151249D",
+                stopLon: "18.395922D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+        def bayamon = new Stop (
+                codeName: "bayamon",
+                stopId: "1",
+                stopCode: "",
+                stopName: "Bayam\u00F3n",
+                stopDesc: "",
+                stopLat:"-66.153815D",
+                stopLon: "18.40095D",
+                zoneId: "",
+                stopUrl: "",
+                locationType: "",
+                parentStation: "",
+                stopTimezone: "",
+                wheelchairBoarding: ""
+        )
+
+
+        stops << sagrado_corazon
+        stops << hato_rey
+        stops << roosevelt
+        stops << domenech
+        stops << pinero
+        stops << universidad
+        stops << rio_piedras
+        stops << cupey
+        stops << centro_medico
+        stops << san_francisco
+        stops << las_lomas
+        stops << martinez_nadal
+        stops << torrimar
+        stops << jardines
+        stops << deportivo
+        stops << bayamon
+
+    }
+
+}
+
  /********/
 
 
@@ -372,194 +666,8 @@ def createZipFile(){
     zipFile.close()
 }
 
-class Agency {
-    String agencyId
-    String agencyName
-    String agencyUrl
-    String agencyTimezone
-    String agencyLang
-    String agencyPhone
-    String agencyFareUrl
-}
 
-class Route {
-    String routeId
-    String agencyId
-    String routeShortName
-    String routeLongName
-    String routeDesc
-    String routeType
-    String routeUrl
-    String routecolor
-    String routeTextColor
-}
 
-class Trip {
-    Stop startStop
-    Stop endStop
-    String routeId
-    String serviceId
-    String tripId
-    String tripHeadsign
-    String tripShortName
-    String directionId
-    String blockId
-    String shapeId
-    String wheelchairAccessible
-}
 
-class TrainArrival {
-    String arrivesTo
-    String direction
-    String daytype
-    String arrivalTime
-}
 
-class StopTime {
-    String tripId
-    String arrivalTime
-    String departureTime
-    String stopId
-    String stopSequence
-}
 
-class WebConnectService {
-
-    final String connectionUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1 (compatible; loteria_pr_bot; +http://www.webninjapr.com/)";
-    final String connectionAccept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
-    final String connectionAcceptLanguage = "en-US,en;q=0.8";
-    final String connectionAcceptCharset = "ISO-8859-1,utf-8;q=0.7,*;q=0.3";
-    final String connectionContentType = "application/x-www-form-urlencoded";
-
-    def doPostConnection(URL url, String requestParameters, ArrayList<String> cookies){
-        DataOutputStream wr;
-        HttpURLConnection connectionPost;
-        //System.setProperty("http.agent", "");
-        def webpage=null
-        def retCookies=null
-        try {
-            connectionPost = url.openConnection();
-            connectionPost.setConnectTimeout(10000)
-            connectionPost.setRequestMethod("POST");
-            connectionPost.setDoOutput(true);
-            connectionPost.setDoInput(true);
-            connectionPost.setRequestProperty("User-Agent", connectionUserAgent);
-            connectionPost.setRequestProperty("Accept", connectionAccept);
-            //connectionPost.setRequestProperty(Accept-Encoding:gzip,deflate,sdch)
-            connectionPost.setRequestProperty("Accept-Language",connectionAcceptLanguage);
-            connectionPost.setRequestProperty("Accept-Charset", connectionAcceptCharset);
-            //connectionPost.setRequestProperty(java.net.URLEncoder.encode("Cache-Control", "ISO-8859-1"),java.net.URLEncoder.encode("max-age=0", "ISO-8859-1"));
-            connectionPost.setRequestProperty("Content-Type", connectionContentType);
-            if(cookies && cookies.size()>0){
-                connectionPost.setRequestProperty("Cookie", cookies.join("; "));
-            }
-            connectionPost.setRequestProperty("Content-Length",requestParameters.getBytes().length.toString());
-
-            connectionPost.connect();
-            //Send request
-            wr = new DataOutputStream (
-                    connectionPost.getOutputStream ());
-            wr.writeBytes (requestParameters);
-            wr.flush ();
-
-            //From http://www.hccp.org/java-net-cookie-how-to.html
-            String headerName=null;
-            ArrayList<String> returnCookies = [];
-            for (int i=1; (headerName = connectionPost.getHeaderFieldKey(i))!=null; i++) {
-                if (headerName.equals("Set-Cookie")) {
-                    String cookie = connectionPost.getHeaderField(i);
-
-                    def cookieCutter = cookie.split(";\\s?")
-
-                    returnCookies.addAll(cookieCutter);
-                    //String cookieName = cookie.substring(0, cookie.indexOf("="));
-                    //String cookieValue = cookie.substring(cookie.indexOf("=") + 1, cookie.length());
-                }
-            }
-
-            webpage = connectionPost.content.text
-            retCookies = returnCookies
-        } catch (e){
-            log.error(e); //throw new Exception(e);
-        } finally {
-            if(wr){
-                wr.close ();
-            }
-            if(connectionPost){
-                connectionPost.disconnect();
-            }
-            return [webpage: webpage, cookies: retCookies];
-        }
-    }
-
-    def doGetConnection(URL url, String requestParameters){
-        DataOutputStream wr;
-        HttpURLConnection connectionGet;
-        //System.setProperty("http.agent", "");
-        def webpage = null
-        def cookies = null
-        try {
-            connectionGet = url.openConnection();
-            connectionGet.setConnectTimeout(10000)
-            connectionGet.setRequestMethod("GET");
-            connectionGet.setDoOutput(false);
-            connectionGet.setDoInput(true);
-            connectionGet.setRequestProperty("User-Agent", connectionUserAgent);
-            connectionGet.setRequestProperty("Accept", connectionAccept);
-            //connectionGet.setRequestProperty(Accept-Encoding:gzip,deflate,sdch)
-            connectionGet.setRequestProperty("Accept-Language",connectionAcceptLanguage);
-            connectionGet.setRequestProperty("Accept-Charset", connectionAcceptCharset);
-            //connectionGet.setRequestProperty(java.net.URLEncoder.encode("Cache-Control", "ISO-8859-1"),java.net.URLEncoder.encode("max-age=0", "ISO-8859-1"));
-            connectionGet.setRequestProperty("Content-Type", connectionContentType);
-            connectionGet.setRequestProperty("Content-Length",requestParameters.getBytes().length.toString());
-
-            connectionGet.connect();
-
-            //From http://www.hccp.org/java-net-cookie-how-to.html
-            String headerName=null;
-            ArrayList<String> returnCookies = [];
-            for (int i=1; (headerName = connectionGet.getHeaderFieldKey(i))!=null; i++) {
-                if (headerName.equals("Set-Cookie")) {
-                    String cookie = connectionGet.getHeaderField(i);
-
-                    def cookieCutter = cookie.split(";\\s?")
-
-                    returnCookies.addAll(cookieCutter);
-                    //String cookieName = cookie.substring(0, cookie.indexOf("="));
-                    //String cookieValue = cookie.substring(cookie.indexOf("=") + 1, cookie.length());
-                }
-            }
-
-            webpage = connectionGet.content.text
-            cookies = returnCookies
-        } catch (e){
-            log.error(e) //throw new Exception(e);
-        } finally {
-            if(wr){
-                wr.close ();
-            }
-            if(connectionGet){
-                connectionGet.disconnect();
-            }
-            return [webpage: webpage, cookies: cookies];
-        }
-
-    }
-
-}
-
-class TrenXmlParserFactory {
-    static def parser=null
-    static def xmlParser=null
-
-    static def getXmlParser(){
-        if(!parser){
-            parser = new org.cyberneko.html.parsers.SAXParser()
-            parser.setFeature('http://xml.org/sax/features/namespaces', false)
-        }
-        if(!xmlParser){
-            xmlParser = new XmlParser(parser)
-        }
-        return xmlParser;
-    }
-}
